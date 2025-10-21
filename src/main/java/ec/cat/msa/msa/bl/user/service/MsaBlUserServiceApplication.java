6b41d1a285f51f688dbcc668f58e3ac5b7ab2837
mateoap00 <mateo.a.p.2000@outlook.es>
2025-10-20 20:58:00 -0500

    configuracion inicial

---
+++ src/main/java/ec/cat/msa/msa/bl/user/service/MsaBlUserServiceApplication.java
@@ -0,0 +1,13 @@
+package ec.cat.msa.msa.bl.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaBlUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaBlUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/cat/msa/msa/bl/user/service/MsaBlUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.cat.msa.msa.bl.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaBlUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
